2599586efe11839eb600ebc87fa5ce6fcdc3a826
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 16:36:44 +0530

    Initial Commit

---
+++ iCalc/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iCalc
+//
+//  Created by HARDIK SHARMA on 07/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCalc/iCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalcApp.swift
+//  iCalc
+//
+//  Created by HARDIK SHARMA on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
